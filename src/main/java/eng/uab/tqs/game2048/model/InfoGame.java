be360ad86275c7108e22b044b57ad4a0fc852894
Adrián Valverde <contactovalverde42@gmail.com>
2025-10-19 11:23:04 +0200

    InfoGame class Done

--- src/main/java/eng/uab/tqs/game2048/model/InfoGame.java
+++ src/main/java/eng/uab/tqs/game2048/model/InfoGame.java
@@ -1,4 +1,35 @@
 package eng.uab.tqs.game2048.model;
 public class InfoGame {
+  public static enum Key {
+    UP, DOWN, LEFT, RIGHT
+  }
 
+  public static enum Color{
+    GREEN(1),
+    PINK(2),
+    YELLOW(3),
+    ORANGE(4),
+    RED(5),
+    BLUE(6),
+    PURPLE(7),
+    BROWN(8),
+    GREY(9),
+    NAVY(10),
+    BLACK(11),
+    NONE(0);
+
+    private final int value;
+
+    Color(int value) {
+      this.value = value;
+    }
+
+    public int getValue() {
+      return value;
+    }
+  }
+
+  public final static int N_COL_BOARD = 4;
+  public final static int N_ROW_BOARD = 4;
+  public final static int SIZE = 4;
 }
