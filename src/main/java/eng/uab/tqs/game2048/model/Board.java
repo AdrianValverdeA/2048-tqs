e235d7b3073b98ef4796f737d658f641788bb1ff
Adrián Valverde <contactovalverde42@gmail.com>
2025-11-23 20:51:59 +0100

    externalitzacio del problema random, mock generator

--- src/main/java/eng/uab/tqs/game2048/model/Board.java
+++ src/main/java/eng/uab/tqs/game2048/model/Board.java
@@ -1,11 +1,15 @@
 package eng.uab.tqs.game2048.model;
+import java.util.ArrayList;
 import java.util.Random;
+
+import static eng.uab.tqs.game2048.model.Generator.genRandom;
+import static eng.uab.tqs.game2048.model.InfoGame.SIZE;
 
 public class Board {
 
-  private final int SIZE = 4;
   private Block[][] board;
   private int score;
+  private Generator gen;
 
   public Board() {
     board = new Block[SIZE][SIZE];
@@ -37,10 +41,21 @@ public class Board {
   }
 
   public void randomInicialize() {
-    for (int i = 0; i < 2; i++) {
-      int x = new Random().nextInt(SIZE);
-      int y = new Random().nextInt(SIZE);
-      board[x][y] = new Block(2, InfoGame.Color.GREEN);
-    }
+    int x1, y1, x2, y2;
+
+    x1 = genRandom();
+    y1 = genRandom();
+
+    do {
+      x2 = genRandom();
+      y2 = genRandom();
+    } while (x1 == x2 && y1 == y2);
+
+    board[x1][y1] = new Block(2, InfoGame.Color.GREEN);
+    board[x2][y2] = new Block(2, InfoGame.Color.GREEN);
+  }
+
+  public void setGenerator(Generator gen) {
+    this.gen = gen;
   }
 }
---
+++ src/main/java/eng/uab/tqs/game2048/model/Generator.java
@@ -0,0 +1,13 @@
+package eng.uab.tqs.game2048.model;
+
+import java.util.Random;
+
+import static eng.uab.tqs.game2048.model.InfoGame.SIZE;
+
+public class Generator {
+
+  public static int genRandom() {
+    int x = new Random().nextInt(SIZE);
+    return x;
+  }
+}
