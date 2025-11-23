0da60fa410fd85199949cfa92e97667b64a655bb
Adrián Valverde <contactovalverde42@gmail.com>
2025-11-23 18:09:40 +0100

    Block Class Done

--- src/main/java/eng/uab/tqs/game2048/model/Block.java
+++ src/main/java/eng/uab/tqs/game2048/model/Block.java
@@ -4,5 +4,26 @@ public class Block
 {
   private int value;
   private InfoGame.Color color;
-  
+
+  public Block()
+  {
+    this.value = 0;
+    this.color = InfoGame.Color.NONE;
+  }
+
+  public Block(int value, InfoGame.Color color)
+  {
+    this.value = value;
+    this.color = color;
+  }
+
+  public int getValue()
+  {
+    return this.value;
+  }
+
+  public InfoGame.Color getColor()
+  {
+    return this.color;
+  }
 }
