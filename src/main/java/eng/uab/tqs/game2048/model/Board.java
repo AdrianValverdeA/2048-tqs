6a044a822a6c3e8fd2720d35b8cf1d8c101cd623
Adrián Valverde <contactovalverde42@gmail.com>
2025-11-23 19:28:52 +0100

    Board Done

--- src/main/java/eng/uab/tqs/game2048/model/Board.java
+++ src/main/java/eng/uab/tqs/game2048/model/Board.java
@@ -3,5 +3,36 @@ import java.util.Random;
 
 public class Board {
 
+  private final int SIZE = 4;
+  private Block[][] board;
+  private int score;
 
+  public Board() {
+    board = new Block[SIZE][SIZE];
+    score = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        board[i][j] = new Block(0, InfoGame.Color.NONE);
+      }
+    }
+
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public Block[][] getBoard() {
+    return board;
+  }
+
+  public void drawBoard() {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        System.out.print(board[i][j].getValue());
+      }
+      System.out.println("");
+    }
+  }
 }
