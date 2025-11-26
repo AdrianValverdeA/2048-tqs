cbf31399d2c7b8b761fb899ba66df6fa9e3e57ed
Adrián Valverde Ambrosio <1707952@uab.cat>
2025-11-26 09:12:06 +0100

    update moveblock test, forgot to check colors, coords and restart board object

--- src/test/java/eng/uab/tqs/game2048/model/BoardTest.java
+++ src/test/java/eng/uab/tqs/game2048/model/BoardTest.java
@@ -65,48 +65,72 @@ class BoardTest {
     board.setGenerator(gen);
     board.randomInicialize();
     Block[][] b = board.getBoard();
-    assertEquals(b[0][2].getValue(),0);
-    board.moveBlock(1,2,"w"); //up
+    int[] newCoords = board.moveBlock(1,2,"w"); //up
     b = board.getBoard();
     assertEquals(b[0][2].getValue(),2);
+    assertEquals(b[0][2].getColor(),InfoGame.Color.GREEN);
     assertEquals(b[1][2].getValue(),0);
+    assertEquals(b[1][2].getColor(),InfoGame.Color.NONE);
+    assertEquals(newCoords[0], 0);
+    assertEquals(newCoords[1], 2);
 
+    board = new Board();
+    board.setGenerator(gen);
     board.randomInicialize();
     b = board.getBoard();
-    assertEquals(b[1][1].getValue(),0);
-    board.moveBlock(1,2,"a"); //left
+    newCoords = board.moveBlock(1,2,"a"); //left
     b = board.getBoard();
     assertEquals(b[1][1].getValue(),2);
+    assertEquals(b[1][1].getColor(),InfoGame.Color.GREEN);
     assertEquals(b[1][2].getValue(),0);
+    assertEquals(b[1][2].getColor(),InfoGame.Color.NONE);
+    assertEquals(newCoords[0], 1);
+    assertEquals(newCoords[1], 1);
 
+    board = new Board();
+    board.setGenerator(gen);
     board.randomInicialize();
     b = board.getBoard();
-    assertEquals(b[2][2].getValue(),0);
-    board.moveBlock(1,2,"s"); //down
+    newCoords = board.moveBlock(1,2,"s"); //down
     b = board.getBoard();
     assertEquals(b[2][2].getValue(),2);
+    assertEquals(b[2][2].getColor(),InfoGame.Color.GREEN);
     assertEquals(b[1][2].getValue(),0);
+    assertEquals(b[1][2].getColor(),InfoGame.Color.NONE);
+    assertEquals(newCoords[0], 0);
+    assertEquals(newCoords[1], 2);
+    assertEquals(newCoords[0], 2);
+    assertEquals(newCoords[1], 2);
 
+    board = new Board();
+    board.setGenerator(gen);
     board.randomInicialize();
     b = board.getBoard();
-    assertEquals(b[1][3].getValue(),0);
-    board.moveBlock(1,2,"d"); //right
+    newCoords = board.moveBlock(1,2,"d"); //right
     b = board.getBoard();
     assertEquals(b[1][3].getValue(),2);
+    assertEquals(b[1][3].getColor(),InfoGame.Color.GREEN);
     assertEquals(b[1][2].getValue(),0);
+    assertEquals(b[1][2].getColor(),InfoGame.Color.NONE);
+    assertEquals(newCoords[0], 1);
+    assertEquals(newCoords[1], 3);
 
+    board = new Board();
+    board.setGenerator(gen);
     board.randomInicialize();
     try {
-      board.moveBlock(3,3,"d"); //right
+      newCoords = board.moveBlock(3,3,"d"); //right
     } catch (Exception e) {
       fail("Out of index");
     }
     b = board.getBoard();
     assertEquals(b[3][3].getValue(),2);
 
+    board = new Board();
+    board.setGenerator(gen);
     board.randomInicialize();
     try {
-      board.moveBlock(3,3,"s"); //down
+      newCoords = board.moveBlock(3,3,"s"); //down
     } catch (Exception e) {
       fail("Out of index");
     }
