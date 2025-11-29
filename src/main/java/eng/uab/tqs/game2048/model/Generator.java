73da96c201f9a3af18fed13fd8605f5934af6660
Adrián Valverde Ambrosio <1707952@uab.cat>
2025-11-29 19:28:58 +0100

    Generator change, random initialize implemented

--- src/main/java/eng/uab/tqs/game2048/model/Generator.java
+++ src/main/java/eng/uab/tqs/game2048/model/Generator.java
@@ -10,4 +10,19 @@ public class Generator {
     int x = new Random().nextInt(SIZE);
     return x;
   }
+
+  public void randomInicialize(Block[][] board) {
+    int x1, y1, x2, y2;
+
+    x1 = genRandom();
+    y1 = genRandom();
+
+    do {
+      x2 = genRandom();
+      y2 = genRandom();
+    } while (x1 == x2 && y1 == y2);
+
+    board[x1][y1] = new Block(2, InfoGame.Color.GREEN);
+    board[x2][y2] = new Block(2, InfoGame.Color.GREEN);
+  }
 }
