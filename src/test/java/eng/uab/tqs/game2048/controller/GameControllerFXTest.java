1e108072981bed41eb138151ff681a93fc1dbae2
Adrián Valverde Ambrosio <1707952@uab.cat>
2025-12-08 00:14:46 +0100

    gamecontrollerfx tests done

---
+++ src/test/java/eng/uab/tqs/game2048/controller/GameControllerFXTest.java
@@ -0,0 +1,37 @@
+package eng.uab.tqs.game2048.controller;
+
+import eng.uab.tqs.game2048.model.mock.GameMock;
+import eng.uab.tqs.game2048.view.mock.GameViewFXMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerFXTest {
+
+  @Test
+  void testOnStartGame() {
+    GameMock model = new GameMock();
+    GameViewFXMock view = new GameViewFXMock();
+
+    GameControllerFX controller = new GameControllerFX(model, view);
+    controller.setTestMode(true);
+    controller.onStartGame();
+
+    assertTrue(model.playGameCalled);
+    assertTrue(view.showGameCalled);
+    assertNotNull(view.lastBoard);
+  }
+
+  @Test
+  void testOnShowScores() {
+    GameMock model = new GameMock();
+    GameViewFXMock view = new GameViewFXMock();
+
+    GameControllerFX controller = new GameControllerFX(model, view);
+
+    controller.onShowScores();
+
+    assertTrue(view.showScoresCalled);
+    assertEquals("Alice:1000", view.lastScores.get(0));
+  }
+}
