18a8c7260884aa148a82a19c5205f9f03e64fe8b
Adrián Valverde Ambrosio <1707952@uab.cat>
2025-12-07 18:04:06 +0100

    GameTest done showScores checked saveScore done taking into account the rankings menu checked, excepts 1 option that is impossible to only check because it starts a gameMatch and leads to infinite loop so it is checked on save and show score

---
+++ src/test/java/eng/uab/tqs/game2048/model/GameTest.java
@@ -0,0 +1,93 @@
+package eng.uab.tqs.game2048.model;
+
+import eng.uab.tqs.game2048.model.mock.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+  Game game;
+  FileManagerMock fm;
+  ScannerGameMock scan;
+  ScannerMovementMock scannerMovement;
+  GeneratorMock generator;
+
+  @BeforeEach
+  void setUp() {
+    game = new Game();
+    fm = new FileManagerMock();
+    scan = new ScannerGameMock();
+    game.setManager(fm);
+    game.setScanner(scan);
+    scannerMovement = new ScannerMovementMock();
+    generator = new GeneratorMock();
+  }
+
+  @Test
+  void saveScoreWithout() throws Exception {
+    fm.setConfig("EMPTY_FILE");
+    scan.setConfig("inputName");
+    BoardMock board = new BoardMock();
+    board.setScore(400);
+    GameMatch gm = game.getGameMatch();
+    scannerMovement.setConfig("d");
+    gm.setScanner(scannerMovement);
+    board.setGenerator(generator);
+    generator.setConfig("game_match_win");
+    board.setConfig("default");
+    gm.setBoard(board);
+    game.playGame();
+
+    fm.setConfig("ONE_SCORE");
+    game.showScores();
+
+    List<String> s = game.getScores();
+    assertEquals(1, s.size());
+    assertEquals("TestUser:400", s.get(0));
+  }
+
+  @Test
+  void saveScoreWith2() throws Exception {
+    scan.setConfig("inputName");
+    BoardMock board = new BoardMock();
+    board.setScore(2000);
+    GameMatch gm = game.getGameMatch();
+    scannerMovement.setConfig("d");
+    gm.setScanner(scannerMovement);
+    board.setGenerator(generator);
+    generator.setConfig("game_match_win");
+    board.setConfig("default");
+    gm.setBoard(board);
+    game.playGame();
+
+    fm.setConfig("TWO_SCORES");
+    game.showScores();
+
+    List<String> s = game.getScores();
+
+    assertEquals(3, s.size());
+    assertEquals("TestUser:2000", s.get(0));
+    assertEquals("Alice:1500", s.get(1));
+    assertEquals("Bob:900", s.get(2));
+  }
+
+  @Test
+  void menu2() throws Exception {
+    assertEquals(game.getScores(), null);
+    scan.setConfig("2");
+    game.menu();
+    assertNotEquals(game.getScores(), null);
+  }
+
+  @Test
+  void menu3() throws Exception {
+    assertFalse(game.getExit());
+    scan.setConfig("3");
+    game.menu();
+    assertTrue(game.getExit());
+  }
+}
