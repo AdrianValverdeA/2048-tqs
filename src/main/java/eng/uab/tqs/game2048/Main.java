37f2ae2abaae585a977d474733d277d80e18a53e
Adrián Valverde <contactovalverde42@gmail.com>
2025-12-08 19:56:09 +0100

    comment added

--- src/main/java/eng/uab/tqs/game2048/Main.java
+++ src/main/java/eng/uab/tqs/game2048/Main.java
@@ -2,7 +2,7 @@ package eng.uab.tqs.game2048;
 
 public class Main {
   public static void main(String[] args) {
-    // Esto arranca JavaFX sin problemas de módulos
+    // start main from FX, view game
     MainFX.main(args);
   }
 }
